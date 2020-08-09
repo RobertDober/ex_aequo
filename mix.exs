b9ba2963d62f5df3d554e1e656bd656e422970ce
RobertDober <robert.dober@gmail.com>
2020-08-09 11:05:00 +0200

    removed /priv

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExAequo.Mixfile do
   Some Tools Commonly Needed (commonly means once a year by me, but still)
   """
   @url "https://github.com/robertdober/ex_aequo"
-  @version "0.1.3"
+  @version "0.1.5"
   def project do
     [
      aliases: [docs: &build_docs/1],
