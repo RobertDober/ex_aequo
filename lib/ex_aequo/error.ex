07fb91cb596c61d724de983674ff38369695c55d
RobertDober <robert.dober@gmail.com>
2021-08-24 17:35:15 +0200

    preparing clean slate for v0.2.0

--- lib/ex_aequo/error.ex
+++ lib/ex_aequo/error.ex
@@ -2,7 +2,9 @@ defmodule ExAequo.Error do
   @moduledoc false
 
   defexception [:message]
-  
+
+  @type t :: %__MODULE__{__exception__: true, message: String.t}
+
   @doc false
   @spec exception( String.t ) :: Exception.t
   def exception(msg), do: %__MODULE__{message: msg}
